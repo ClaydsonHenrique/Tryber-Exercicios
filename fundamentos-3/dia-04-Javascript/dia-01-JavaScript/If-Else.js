1318f65a4fc952dde2d749531db62acc85ee0c6f
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-11-30 14:10:22 -0300

    terminando if e else

---
+++ fundamentos-3/dia-04-Javascript/dia-01-JavaScript/If-Else.js
@@ -0,0 +1,27 @@
+const nota = 81;
+if (nota >= 80 ){
+  console.log('Parabéns,você faz parte do grupo das pessoas aprovadas');
+} else if (nota < 80 && nota >= 60) {
+console.log('Você está na nossa lista de espera');
+} else if (nota <60){
+  console.log("Infelizmente, você reprovou");
+}
+
+const nota2 = 60;
+if (nota2 >= 80 ){
+  console.log('Parabéns,você faz parte do grupo das pessoas aprovadas')
+} else if (nota2 < 80 && nota2 >= 60) {
+console.log('Você está na nossa lista de espera');
+}else if (nota2 <60){
+  console.log("Infelizmente, você reprovou");
+}
+
+const nota3 = 32;
+if (nota3 >= 80 ){
+  console.log('Parabéns,você faz parte do grupo das pessoas aprovadas');
+} else if (nota3 < 80 && nota3 >= 60) {
+console.log('Você está na nossa lista de espera');
+}else if (nota3 <60){
+  console.log("Infelizmente, você reprovou");
+}
+
