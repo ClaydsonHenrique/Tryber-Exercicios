3e52385cef45142857984481ae6242448aff9bcc
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-11-30 13:33:13 -0300

    terminando conteudo Primeiros-Passos

--- fundamentos-3/dia-04-Javascript/dia-01-JavaScript/variaveis.js
+++ fundamentos-3/dia-04-Javascript/dia-01-JavaScript/variaveis.js
@@ -3,3 +3,11 @@ const birthCity = 'Luziânia';
 let birthYear = 1999;
 console.log(myName, birthCity, birthYear);
 
+birthYear= 2030;
+console.log(birthYear)
+
+birthCity = 'Gama';
+console.log(birthCity);
+
+// O erro aconteceu porque o const não pode ser alterada depois que foi dada o valor. Para ultizar uma variavel que seu valor possa ser alterado futuramente é usado var ou let . // 
+ 
