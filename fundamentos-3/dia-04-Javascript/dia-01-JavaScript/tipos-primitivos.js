4414df140c4d92a64debe4c9a2134ad9a59dc31a
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-11-30 13:47:12 -0300

    terminando tipos primitivos

---
+++ fundamentos-3/dia-04-Javascript/dia-01-JavaScript/tipos-primitivos.js
@@ -0,0 +1,20 @@
+// exemplo do Tipos primitivos
+
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+// console.log(typeof patientId)
+
+
+// Pra fixar
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+console.log(area);
+
+const perimeter = 5+5+8;
+console.log(perimeter);
