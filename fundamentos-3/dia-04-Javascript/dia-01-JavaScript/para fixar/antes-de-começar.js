b4fc33f027c2e4ef0604d9d8aedf92d99c02510a
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-04 21:41:54 -0300

    primeiro requisito

---
+++ fundamentos-3/dia-04-Javascript/dia-01-JavaScript/para fixar/antes-de-começar.js
@@ -0,0 +1,29 @@
+// soma 
+const number = 3;
+const number2 = 6;
+const soma = number + number2;
+console.log(soma);
+
+// subitração 
+const numero = 5;
+const numero2 = 3;
+const subitracao = numero - numero2;
+console.log(subitracao);
+
+// multiplicação 
+const multi = 4;
+const multi2 = 10;
+const multiplicacao = multi * multi2 ;
+console.log(multiplicacao);
+
+// divisão 
+const dividendo = 40;
+const divisor = 4;
+const divisao = dividendo / divisor;
+console.log(divisao)
+
+// modulo 
+const dividendo2 = 30;
+const divisor2 = 2;
+const resultado = 30 % 2;
+console.log(resultado);
