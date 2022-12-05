f8559cfb870d95f32ce017aa22f803daee982539
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-05 01:17:01 -0300

    terceiro requisisto finalizado

--- fundamentos-3/dia-04-Javascript/dia-02-JavaScript/Exercicio-dia-02.js
+++ fundamentos-3/dia-04-Javascript/dia-02-JavaScript/Exercicio-dia-02.js
@@ -3,9 +3,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for ( index=0; index < numbers.length; index += 1){
   // console.log(numbers[index])
 }
+
 // Some todos os valores contidos no array e imprima o resultado;
 let soma= 0;
 for ( index=0; index < numbers.length; index += 1){
   soma += numbers[index];
 }
-console.log(soma)
+// console.log(soma)
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+let aritimetica = soma / numbers.length;
+console.log(aritimetica)
