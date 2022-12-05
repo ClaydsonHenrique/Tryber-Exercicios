ac501a9771382fe54ebc1904d052ce3a69009d69
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-05 00:48:43 -0300

    finalizando for

--- fundamentos-3/dia-04-Javascript/dia-02-JavaScript/arrays-lista.js
+++ fundamentos-3/dia-04-Javascript/dia-02-JavaScript/arrays-lista.js
@@ -1,19 +1,27 @@
-// requisito 01 
+// Requisito 01 
 // Obtenha o valor “Serviços” do array menu:
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 console.log(menuServices);
 
-// requisito 02
+// Requisito 02
 // Procure o índice do valor “Portfólio” do array menu:
 menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu[2];
 console.log(indexOfPortfolio);
 
-// requisito 03
+// Requisito 03
 // Adicione o valor “Contato” no final do array menu:
 menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu.push("Contato");
 
 console.log(menu);
+
+// Requisito 04 For 
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for ( index=0; index < groceryList.length; index += 1){
+  console.log(groceryList[index]);
+}
