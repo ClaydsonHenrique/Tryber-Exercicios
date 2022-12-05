13323da44c49d2e596b55dfeb7be25e3e4299d75
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-05 01:14:33 -0300

    segundo requisito finalizado

--- fundamentos-3/dia-04-Javascript/dia-02-JavaScript/Exercicio-dia-02.js
+++ fundamentos-3/dia-04-Javascript/dia-02-JavaScript/Exercicio-dia-02.js
@@ -1,5 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
 for ( index=0; index < numbers.length; index += 1){
-  console.log(numbers[index])
+  // console.log(numbers[index])
 }
+// Some todos os valores contidos no array e imprima o resultado;
+let soma= 0;
+for ( index=0; index < numbers.length; index += 1){
+  soma += numbers[index];
+}
+console.log(soma)
