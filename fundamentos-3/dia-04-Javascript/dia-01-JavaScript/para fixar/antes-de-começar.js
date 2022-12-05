69a8cc2b879d045c6be1955182303eb70c5f7ede
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-04 22:01:56 -0300

    terceiro requisito completo

--- fundamentos-3/dia-04-Javascript/dia-01-JavaScript/para fixar/antes-de-começar.js
+++ fundamentos-3/dia-04-Javascript/dia-01-JavaScript/para fixar/antes-de-começar.js
@@ -1,34 +1,34 @@
-// soma 
+// // soma 
 const number = 3;
 const number2 = 6;
 const soma = number + number2;
 console.log(soma);
 
-// subitração 
+// // subitração 
 const numero = 5;
 const numero2 = 3;
 const subitracao = numero - numero2;
 console.log(subitracao);
 
-// multiplicação 
+// // multiplicação 
 const multi = 4;
 const multi2 = 10;
 const multiplicacao = multi * multi2 ;
 console.log(multiplicacao);
 
-// divisão 
+// // divisão 
 const dividendo = 40;
 const divisor = 4;
 const divisao = dividendo / divisor;
 console.log(divisao)
 
-// modulo 
+// // modulo 
 const dividendo2 = 30;
 const divisor2 = 2;
 const resultado = 30 % 2;
 console.log(resultado);
 
-// Resultado correto (gabarito)
+// // Resultado correto (gabarito)
 
 const a = 15;
 const b = 5;
@@ -48,4 +48,14 @@ if (primeiro > segundo){
   console.log("Segundo é maior que Primeiro")
 }
 
+primeiro;
+segundo;
+const terceiro = 35;
+if (primeiro > segundo && primeiro > terceiro){
+  console.log("Primeiro é maior que segundo e do que o Terceiro");
+}else if (segundo > primeiro && segundo > terceiro){
+  console·log("Segundo é maior que primeiro e do que o Terceiro");
+} else{
+  console.log("Terceiro é maior que Primeiro e do que o Segundo");
+}
 
