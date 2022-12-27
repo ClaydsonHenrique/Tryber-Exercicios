3c5f74d153e8c81badbbbb4941b94285dbe21520
Claydson Henrique <claydsonhenriquebs@gmail.com>
2022-12-26 21:38:43 -0300

    quarto requisito

--- fundamentos-3/04-JavaScript/dia-03-javaScript/bonus.js
+++ fundamentos-3/04-JavaScript/dia-03-javaScript/bonus.js
@@ -39,3 +39,28 @@ for (let index = 0; index <= n; index += 1) {
   novaLinha = '';
   position -= 1;
 }
+
+// quarto requisito 
+let quantidade = 5;
+let caractere = '*';
+let novasLinhas = '';
+
+let meio = (quantidade + 1) / 2;
+let esquerda = meio;
+let direita = meio;
+
+for (let index = 0; index <= meio; index += 1) {
+  for (let i = 0; i <= quantidade; i += 1) {
+    if (i > esquerda && i < direita) {
+      novasLinhas = novasLinhas + caractere;
+    } else {
+      novasLinhas = novasLinhas + ' ';
+    };
+  };
+  console.log(novasLinhas);
+  novasLinhas = '';
+  direita += 1;
+  esquerda -= 1;
+};
+
+
